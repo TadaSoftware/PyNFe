043d63b1ebd4963084c9fd19e19ef62e8fca3bf4
Marinho Brandão <marinho@gmail.com>
2010-01-11 15:15:34 -0200

    Separando testes das entidades

--- pynfe/entidades/emitente.py
+++ pynfe/entidades/emitente.py
@@ -1,5 +1,56 @@
 from base import Entidade
 
 class Emitente(Entidade):
-    pass
+    # Dados do Emitente
+    # - Nome/Razao Social (obrigatorio)
+    razao_social = str()
+
+    # - Nome Fantasia
+    nome_fantasia = str()
+
+    # - CNPJ (obrigatorio)
+    cnpj = str()
+
+    # - Inscricao Estadual (obrigatorio)
+    inscricao_estadual = str()
+
+    # - CNAE Fiscal
+    cnae_fiscal = str()
+
+    # - Inscricao Municipal
+    inscricao_municipal = str()
+
+    # - Inscricao Estadual (Subst. Tributario)
+    inscricao_estadual_subst_tributaria = str()
+
+    # Endereco
+    # - Logradouro (obrigatorio)
+    endereco_logradouro = str()
+
+    # - Numero (obrigatorio)
+    endereco_numero = str()
+
+    # - Complemento
+    endereco_complemento = str()
+
+    # - Bairro (obrigatorio)
+    endereco_bairro = str()
+
+    # - CEP
+    endereco_cep = str()
+
+    # - Pais (aceita somente Brasil)
+    endereco_pais = 'BRASIL'
+
+    # - UF (obrigatorio)
+    endereco_uf = str()
+
+    # - Municipio (obrigatorio)
+    endereco_municipio = str()
+
+    # - Telefone
+    endereco_telefone = str()
+
+    # Logotipo
+    logotipo = None
 
