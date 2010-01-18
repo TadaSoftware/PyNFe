5fce583aa52359e458a81829032dd99187c73984
Italo Maia <italomaia@ymail.com>
2010-01-17 22:07:55 -0300

    so testando. Adicionei um import

--- pynfe/processamento/validacao.py
+++ pynfe/processamento/validacao.py
@@ -1,3 +1,26 @@
+#-*- coding:utf-8 -*-
+
+try:
+    from lxml import etree
+except ImportError:
+    try:
+        # Python 2.5 - cElementTree
+        import xml.etree.cElementTree as etree
+    except ImportError:
+        try:
+            # Python 2.5 - ElementTree
+            import xml.etree.ElementTree as etree
+        except ImportError:
+            try:
+                # Instalacao normal do cElementTree
+                import cElementTree as etree
+            except ImportError:
+                try:
+                    # Instalacao normal do ElementTree
+                    import elementtree.ElementTree as etree
+                except ImportError:
+                    raise Exception('Falhou ao importar lxml/ElementTree')
+
 class Validacao(object):
     pass
 
