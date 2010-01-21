62ad9582c6d34234472fa8a07fa3774733c9dfc2
Marinho Brandão <marinho@gmail.com>
2010-01-21 17:38:55 -0200

    Trabalhando na comunicação

--- pynfe/processamento/comunicacao.py
+++ pynfe/processamento/comunicacao.py
@@ -1,22 +1,27 @@
 # -*- coding: utf-8 -*-
-
+import datetime
 from httplib import HTTPSConnection, HTTPResponse
 
-from pynfe.utils import etree, StringIO
+from pynfe.utils import etree, StringIO, so_numeros
 from pynfe.utils.flags import NAMESPACE_NFE, NAMESPACE_SOAP, VERSAO_PADRAO
+from pynfe.utils.flags import CODIGOS_ESTADOS, VERSAO_PADRAO
 
 class Comunicacao(object):
     u"""Classe abstrata responsavel por definir os metodos e logica das classes
     de comunicação com os webservices da NF-e."""
 
+    _ambiente = 1   # 1 = Produção, 2 = Homologação
     servidor = None
+    porta = 80
     certificado = None
     certificado_senha = None
 
-    def __init__(self, servidor, certificado, certificado_senha):
+    def __init__(self, servidor, porta, certificado, certificado_senha, homologacao=False):
         self.servidor = servidor
+        self.porta = porta
         self.certificado = certificado
         self.certificado_senha = certificado_senha
+        self._ambiente = homologacao and 2 or 1
 
 class ComunicacaoSefaz(Comunicacao):
     u"""Classe de comunicação que segue o padrão definido para as SEFAZ dos Estados."""
@@ -41,8 +46,8 @@ class ComunicacaoSefaz(Comunicacao):
 
         # Monta XML para envio da requisição
         xml = self._construir_xml_soap(
-                metodo='CadConsultaCadastro',
-                tag_metodo='consultaCadastro',
+                metodo='CadConsultaCadastro', # FIXME
+                tag_metodo='consultaCadastro', # FIXME
                 cabecalho=self._cabecalho_soap(),
                 dados=dados,
                 )
@@ -51,18 +56,66 @@ class ComunicacaoSefaz(Comunicacao):
         retorno = self._post(post, xml, self._post_header())
 
         # Transforma o retorno em etree
-        try:
-            retorno = etree.parse(StringIO(retorno))
-            return retorno
-        except TypeError:
-            pass
+        #retorno = etree.parse(StringIO(retorno))
+
+        return bool(retorno)
 
     def consultar_cadastro(self, instancia):
         #post = '/nfeweb/services/cadconsultacadastro.asmx'
         post = '/nfeweb/services/nfeconsulta.asmx'
 
-    def inutilizar_faixa_numeracao(self, faixa):
-        pass
+    def inutilizar_faixa_numeracao(self, numero_inicial, numero_final, emitente, ano=None,
+            serie='1', justificativa=''):
+        post = '/nfeweb/services/nfestatusservico.asmx'
+
+        # Valores default
+        ano = str(ano or datetime.date.today().year)[-2:]
+        uf = CODIGOS_ESTADOS[emitente.endereco_uf]
+        cnpj = so_numeros(emitente.cnpj)
+
+        # Identificador da TAG a ser assinada formada com Código da UF + Ano (2 posições) +
+        #  CNPJ + modelo + série + nro inicial e nro final precedida do literal “ID”
+        id_unico = 'ID%(uf)s%(ano)s%(cnpj)s%(modelo)s%(serie)s%(num_ini)s%(num_fin)s'%{
+                'uf': uf,
+                'ano': ano,
+                'cnpj': cnpj,
+                'modelo': '55',
+                'serie': serie.zfill(3),
+                'num_ini': str(numero_inicial).zfill(9),
+                'num_fin': str(numero_final).zfill(9),
+                }
+
+        # Monta XML do corpo da requisição # FIXME
+        raiz = etree.Element('inutNFe', xmlns="http://www.portalfiscal.inf.br/nfe", versao="1.07")
+        inf_inut = etree.SubElement(raiz, 'infInut', Id=id_unico) # FIXME
+        etree.SubElement(inf_inut, 'tpAmb').text = str(self._ambiente)
+        etree.SubElement(inf_inut, 'xServ').text = 'INUTILIZAR'
+        etree.SubElement(inf_inut, 'cUF').text = uf
+        etree.SubElement(inf_inut, 'ano').text = ano
+        etree.SubElement(inf_inut, 'CNPJ').text = emitente.cnpj
+        etree.SubElement(inf_inut, 'mod').text = '55'
+        etree.SubElement(inf_inut, 'serie').text = serie
+        etree.SubElement(inf_inut, 'nNFIni').text = str(numero_inicial)
+        etree.SubElement(inf_inut, 'nNFFin').text = str(numero_final)
+        etree.SubElement(inf_inut, 'xJust').text = justificativa
+        dados = etree.tostring(raiz, encoding='utf-8', xml_declaration=True)
+
+        # Efetua assinatura
+        # Monta XML para envio da requisição
+        xml = self._construir_xml_soap(
+                metodo='CadConsultaCadastro', # FIXME
+                tag_metodo='consultaCadastro', # FIXME
+                cabecalho=self._cabecalho_soap(),
+                dados=dados,
+                )
+
+        # Chama método que efetua a requisição POST no servidor SOAP
+        retorno = self._post(post, xml, self._post_header())
+
+        # Transforma o retorno em etree # TODO
+        #retorno = etree.parse(StringIO(retorno))
+
+        return retorno
 
     def _cabecalho_soap(self):
         u"""Monta o XML do cabeçalho da requisição SOAP"""
@@ -99,11 +152,11 @@ class ComunicacaoSefaz(Comunicacao):
         caminho_chave, caminho_cert = self.certificado.separar_arquivo(senha=self.certificado_senha)
 
         # Abre a conexão HTTPS
-        con = HTTPSConnection(self.servidor, key_file=caminho_chave, cert_file=caminho_cert)
+        con = HTTPSConnection(self.servidor, self.porta, key_file=caminho_chave, cert_file=caminho_cert)
 
         try:
             #con.set_debuglevel(100)
-    
+
             con.request(u'POST', post, xml, header)
 
             resp = con.getresponse()
--- run_fake_soap_server.py
+++ run_fake_soap_server.py
@@ -1,24 +1,50 @@
-from soaplib.wsgi_soap import SimpleWSGISoapApp
-from soaplib.service import soapmethod
-from soaplib.serializers.primitive import String, Integer, Array, Null
+# -*- coding: utf-8 -*-
 
-class ServidorNFEFalso(SimpleWSGISoapApp):
-    @soapmethod(String, Integer, _returns=Array(String))
-    def ping(self, nome, vezes):
-        ret = [nome for i in range(vezes)]
-        print ret
-        return ret
+"""Este script deve ser executado com Python 2.6+ e OpenSSL"""
+
+import os
+
+CUR_DIR = os.path.dirname(os.path.abspath(__file__))
+
+#from soaplib.wsgi_soap import SimpleWSGISoapApp
+#from soaplib.service import soapmethod
+#from soaplib.serializers.primitive import String, Integer, Array, Null
+
+#import tornado.wsgi
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+import tornado.options
+
+#class ServidorNFEFalso(SimpleWSGISoapApp):
+#    @soapmethod(String, Integer, _returns=Array(String))
+#    def ping(self, nome, vezes):
+#        ret = [nome for i in range(vezes)]
+#        return ret
+
+class HandlerStatusServico(tornado.web.RequestHandler):
+    def post(self):
+        self.write('<x/>')
 
 if __name__ == '__main__':
     porta = 8080
 
-    # Via Tornado
-    import tornado.wsgi
-    import tornado.httpserver
-    import tornado.ioloop
-    application = ServidorNFEFalso()
-    container = tornado.wsgi.WSGIContainer(application)
-    http_server = tornado.httpserver.HTTPServer(container)
+    # Codigo específico da soaplib
+    #application = ServidorNFEFalso()
+    #container = tornado.wsgi.WSGIContainer(application)
+    #http_server = tornado.httpserver.HTTPServer(container)
+    
+    tornado.options.parse_command_line()
+    application = tornado.web.Application([
+        (r'^/nfeweb/services/nfestatusservico.asmx$', HandlerStatusServico), # Consulta de status do serviço
+        ])
+
+    ssl_options = {
+            'certfile': os.path.join(CUR_DIR, 'tests', 'certificado.pem'),
+            'keyfile': os.path.join(CUR_DIR, 'tests', 'key.pem'),
+            }
+
+    http_server = tornado.httpserver.HTTPServer(application, ssl_options=ssl_options)
     http_server.listen(porta)
     tornado.ioloop.IOLoop.instance().start()
 
