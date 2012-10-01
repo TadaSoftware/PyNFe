0ee6755a010680e70ea44787068427444b65053d
Sergio Oliveira <sergio@tracy.com.br>
2012-10-01 20:44:32 -0300

    Adding setup script

--- setup.py
+++ setup.py
@@ -1,23 +1,14 @@
+#!/usr/bin/env python
+
 import sys, doctest, os, glob
+from setuptools import setup, find_packages
 
-CUR_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, CUR_DIR)
-
-PACKAGES = ('robvm','robsim','robparser','robide')
-
-if __name__ == '__main__':
-    # Packages to test
-    packages = len(sys.argv) > 1 and sys.argv[1:] or PACKAGES
-
-    # Test each package
-    test_files = []
-    for pkg in packages:
-        test_files.append(glob.glob('%s/*.txt' % os.path.join(CUR_DIR, pkg, 'tests')))
-
-    test_files = reduce(lambda a,b: a+b, test_files)
-    test_files = map(lambda i: i[len(CUR_DIR)+1:], test_files)
-
-    # Run the tests
-    for fname in test_files:
-        doctest.testfile(fname)
-
+setup(
+    name='PyNFe',
+    version='0.1-custom',
+    packages=find_packages(),
+    package_data={
+        'pynfe': ['data/**/*.txt'],
+    },
+    zip_safe=False,
+)
