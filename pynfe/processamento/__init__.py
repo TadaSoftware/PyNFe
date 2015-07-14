d6b45e125c94fd903ba8ef8899dbd6c9b9f8449a
Junior Tada <junior_xaxim@yahoo.com.br>
2015-07-13 22:00:37 -0300

    limpa na classe assinatura

--- pynfe/processamento/__init__.py
+++ pynfe/processamento/__init__.py
@@ -1,6 +1,6 @@
-from serializacao import SerializacaoXML
-from validacao import Validacao
+from .serializacao import SerializacaoXML
+from .validacao import Validacao
 #from assinatura import AssinaturaA1
 #from comunicacao import ComunicacaoSefaz
-from danfe import DANFE
+from .danfe import DANFE
 
