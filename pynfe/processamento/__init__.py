5d2ec7645e95ce7106e30aab1ee61390cda8425b
Junior Tada <juniortada@gmail.com>
2018-06-05 14:50:13 -0300

    [REF] corrigido importação de packages

--- pynfe/processamento/__init__.py
+++ pynfe/processamento/__init__.py
@@ -2,5 +2,7 @@ from .serializacao import SerializacaoXML
 from .serializacao import SerializacaoNfse
 from .validacao import Validacao
 from .assinatura import AssinaturaA1
-from .comunicacao import ComunicacaoSefaz
+from .nfe import ComunicacaoNFe
+# from .mdfe import ComunicacaoMDFe
+# from .nfse import ComunicacaoNFSe
 from .danfe import DanfeNfce
