aedcac04762be11f678057030fd8719a44cc9049
Marcelo Salhab Brogliato <msbrogli@gmail.com>
2018-08-10 16:30:38 -0300

    Add XSDs to package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version='0.4',
     packages=find_packages(),
     package_data={
-        'pynfe': ['data/**/*.txt'],
+        'pynfe': ['data/**/*.txt', 'data/**/*.xsd'],
     },
     install_requires=requirements('requirements.txt'),
     zip_safe=False,
