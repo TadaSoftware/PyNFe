15650a0bd0e33c875a20154f495c15c2c96daa6d
Thiago Montenegro <opencode@gmail.com>
2019-08-01 14:21:05 -0300

    no message

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = lambda f: [str(i.req) for i in parse(f, session=False)]
 
 setup(
     name='PyNFe',
-    version='0.4.1',
+    version='0.4.2',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt'],
