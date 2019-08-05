1c2cefa975ef9430a3ae8b7e83303c3941ff3124
Thiago Montenegro <opencode@gmail.com>
2019-08-05 17:40:15 -0300

    ajuste uf TO  para SVRS

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = lambda f: [str(i.req) for i in parse(f, session=False)]
 
 setup(
     name='PyNFe',
-    version='0.4.4',
+    version='0.4.5',
     packages=find_packages(),
     package_data={
         'pynfe': ['data/**/*.txt'],
