c50fd8c293b5330f765f1eea070746aaeccee868
Leonardo Gregianin <leogregianin@gmail.com>
2020-07-03 13:52:16 -0400

    Criação das entidades do Manifesto

--- pynfe/entidades/__init__.py
+++ pynfe/entidades/__init__.py
@@ -3,9 +3,9 @@ from .produto import Produto
 from .cliente import Cliente
 from .transportadora import Transportadora
 from .notafiscal import NotaFiscal
+from .manifesto import Manifesto
 from .lotes import LoteNotaFiscal
 from .fonte_dados import _fonte_dados
 from .certificado import CertificadoA1
 from .evento import EventoCancelarNota
 from .servico import Servico
-
