c526b55ea97bd2e1eee579ad5b7545bea93e86bd
Leonardo Gregianin <leogregianin@gmail.com>
2022-01-17 15:03:12 -0400

    Melhoria na estrutura dos testes dos certificados

--- tests/tests_certificadoA1.py
+++ tests/tests_certificadoA1.py
@@ -9,12 +9,14 @@ from pynfe.entidades.certificado import CertificadoA1
 class CertificadoTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.certificado = "./tests/certificado.pfx"
-        self.senha = '123456'
+        self.certificado_correto = "./tests/certificado.pfx"
+        self.certificado_incorreto = "./tests/arquivo_erro.pfx"
+        self.senha_correto = '123456'
+        self.senha_incorreto = '654321'
 
     def test_assinatura_com_caminho(self):
-        self.a1 = CertificadoA1(self.certificado)
-        cert = self.a1.separar_arquivo(senha=self.senha, caminho=self.certificado)
+        self.a1 = CertificadoA1(self.certificado_correto)
+        cert = self.a1.separar_arquivo(senha=self.senha_correto, caminho=self.certificado_correto)
 
         self.assertTrue(cert[0].startswith('/tmp/'))
         self.assertTrue(cert[1].startswith('/tmp/'))
@@ -22,26 +24,20 @@ class CertificadoTestCase(unittest.TestCase):
         self.a1.excluir()
 
     def test_exception_certificado_filenotfounderror(self):
-        self.certificado = "./tests/arquivo_erro.pfx"
-        self.senha = '123456'
-
         with self.assertRaises(Exception) as context:
-            self.a1 = CertificadoA1(self.certificado)
-            self.a1.separar_arquivo(senha=self.senha, caminho=self.certificado)
+            self.a1 = CertificadoA1(self.certificado_incorreto)
+            self.a1.separar_arquivo(senha=self.senha_incorreto, caminho=self.certificado_incorreto)
         self.assertEqual(str(context.exception), "Falha ao abrir arquivo do certificado digital A1. Verifique local e permissoes do arquivo.")
 
     def test_exception_certificado_senha_errada(self):
-        self.certificado = "./tests/certificado.pfx"
-        self.senha = '654321'
-
         with self.assertRaises(Exception) as context:
-            self.a1 = CertificadoA1(self.certificado)
-            self.a1.separar_arquivo(senha=self.senha, caminho=self.certificado)
+            self.a1 = CertificadoA1(self.certificado_correto)
+            self.a1.separar_arquivo(senha=self.senha_incorreto, caminho=self.certificado_correto)
         self.assertEqual(str(context.exception), "Falha ao carregar certificado digital A1. Verifique a senha do certificado.")
 
     def test_assinatura_sem_caminho(self):
-        self.a1 = CertificadoA1(self.certificado)
-        cert = self.a1.separar_arquivo(senha=self.senha)
+        self.a1 = CertificadoA1(self.certificado_correto)
+        cert = self.a1.separar_arquivo(senha=self.senha_correto)
 
         texto_inicial_esperado = '-----BEGIN PRIVATE KEY-----'
         texto_inicial_gerado = cert[0].decode('utf-8')
@@ -51,15 +47,15 @@ class CertificadoTestCase(unittest.TestCase):
         self.a1.excluir()
 
     def test_verificar_se_arquivos_temporarios_existem(self):
-        self.a1 = CertificadoA1(self.certificado)
-        self.a1.separar_arquivo(senha=self.senha, caminho=self.certificado)
+        self.a1 = CertificadoA1(self.certificado_correto)
+        self.a1.separar_arquivo(senha=self.senha_correto, caminho=self.certificado_correto)
         self.assertTrue(len(self.a1.arquivos_temp) > 0)
 
         self.a1.excluir()
 
     def test_excluir_arquivos_temporarios(self):
-        self.a1 = CertificadoA1(self.certificado)
-        self.a1.separar_arquivo(senha=self.senha, caminho=self.certificado)
+        self.a1 = CertificadoA1(self.certificado_correto)
+        self.a1.separar_arquivo(senha=self.senha_correto, caminho=self.certificado_correto)
         self.a1.excluir()
 
         self.assertTrue(len(self.a1.arquivos_temp) == 0)
