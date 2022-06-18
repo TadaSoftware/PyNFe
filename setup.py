aa4934defdf00c0da9f555b54020cabb7277d747
juniortada <juniortada@gmail.com>
2022-06-18 14:35:37 -0300

    build first version pip publish close #175

--- setup.py
+++ setup.py
@@ -1,13 +1,32 @@
 #!/usr/bin/env python
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name='PyNFe',
-    version='0.4',
+    version='0.4.1',
     author='TadaSoftware',
     author_email='tadasoftware@gmail.com',
-    url='https://github.com/TadaSoftware',
-    packages=setuptools.find_packages(exclude=['tests', 'tests.*']),
+    description="Interface library with the Brazilian Electronic Invoice web services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/TadaSoftware/PyNFe',
+    project_urls={
+        "Bug Tracker": "https://github.com/TadaSoftware/PyNFe/issues",
+        "Wiki": "https://github.com/TadaSoftware/PyNFe/wiki",
+        "Discussions": "https://github.com/TadaSoftware/PyNFe/discussions"
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(
+        exclude=['tests']),
     package_data={
         'pynfe': ['data/**/*.txt'],
     },
