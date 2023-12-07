dfd3264eb30a8f80a2d2e19b881650c64bc689c3
dfrnks <douglasfrancardoso@gmail.com>
2023-12-07 09:33:13 -0300

    Alterando formato dhEvento do evento

--- pynfe/processamento/serializacao.py
+++ pynfe/processamento/serializacao.py
@@ -1713,8 +1713,9 @@ class SerializacaoXML(Serializacao):
             return raiz
 
     def serializar_evento(self, evento, tag_raiz="evento", retorna_string=False):
-        tz = datetime.now().astimezone().strftime("%z")
-        tz = "{}:{}".format(tz[:-2], tz[-2:])
+        # tz = datetime.now().astimezone().strftime("%z")
+        # tz = "{}:{}".format(tz[:-2], tz[-2:])
+
         raiz = etree.Element(tag_raiz, versao="1.00", xmlns=NAMESPACE_NFE)
         e = etree.SubElement(raiz, "infEvento", Id=evento.identificador)
         etree.SubElement(e, "cOrgao").text = CODIGOS_ESTADOS[evento.uf.upper()]
@@ -1725,8 +1726,9 @@ class SerializacaoXML(Serializacao):
             etree.SubElement(e, "CNPJ").text = evento.cnpj
         etree.SubElement(e, "chNFe").text = evento.chave
         etree.SubElement(e, "dhEvento").text = (
-                evento.data_emissao.strftime("%Y-%m-%dT%H:%M:%S") + tz
+                evento.data_emissao.strftime('%Y-%m-%dT%H:%M:%S%z')
         )
+        # evento.data_emissao.strftime("%Y-%m-%dT%H:%M:%S") + tz
         etree.SubElement(e, "tpEvento").text = evento.tp_evento
         etree.SubElement(e, "nSeqEvento").text = str(evento.n_seq_evento)
         etree.SubElement(e, "verEvento").text = "1.00"
