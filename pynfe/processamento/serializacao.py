a375e178db5f556e6216dcb9c6fbcc70225f958e
dfrnks <douglasfrancardoso@gmail.com>
2024-06-05 16:12:32 -0300

    Remove 99

--- pynfe/processamento/serializacao.py
+++ pynfe/processamento/serializacao.py
@@ -1315,7 +1315,6 @@ class SerializacaoXML(Serializacao):
                     or int(pagamento.tipo_pagamento) == 4
                     or int(pagamento.tipo_pagamento) == 10
                     or int(pagamento.tipo_pagamento) == 11
-                    or int(pagamento.tipo_pagamento) == 99
             ):
                 cartao = etree.SubElement(raiz, "card")
                 """ Tipo de Integração do processo de pagamento com
