604d460e11ceb2f94a336f2ad96f17a0567fdba3
Felipe Correa <eu@felps.dev>
2025-01-08 09:36:49 -0300

    Bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="PyNFe",
-    version="0.4.4",
+    version="0.5.0",
     author="TadaSoftware",
     author_email="tadasoftware@gmail.com",
     description="Interface library with the Brazilian Electronic Invoice web services",
