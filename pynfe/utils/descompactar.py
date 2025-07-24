2db55c3b6d5162b453b0b6128902edea398968bf
WeslenPy <83089496+WeslenPy@users.noreply.github.com>
2025-07-24 18:15:59 -0300

    Update descompactar.py
    forma otimizada de efetuar a descompressão dos dados, a forma anterior retornava "None"

--- pynfe/utils/descompactar.py
+++ pynfe/utils/descompactar.py
@@ -4,8 +4,6 @@
 classe que descompacta o gzip recebido pela consulta distribuicao
 
 """
-
-from io import BytesIO
 import base64
 import gzip
 from lxml import etree
@@ -13,18 +11,12 @@ from lxml import etree
 
 class DescompactaGzip(object):
     @staticmethod
-    def descompacta(stringZipada):
+    def descompacta(stringZipada:bytes):
         """
         :paramn stringZipada: String
 
         :return : Etree
         """
-        arq = BytesIO()
-        arq.write(base64.b64decode(stringZipada))
-        arq.seek(0)
-        zip = gzip.GzipFile(fileobj=arq)
-        texto = zip.read()
-        arq.close()
-        zip.close()
-        descompactado = texto.decode("utf-8").encode()
-        return etree.fromstring(descompactado)
+        decoded = b64decode(stringZipada)
+        decompress_nfe = gzip.decompress(decoded).decode("utf8")
+        return etree.fromstring(decompress_nfe)
